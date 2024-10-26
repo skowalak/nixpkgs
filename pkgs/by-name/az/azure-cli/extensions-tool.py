8e1a55adab901a5a491b8889a44590da9c2fa753
Sebastian Kowalak <skowalak@phoenixcontact-sb.io>
2024-10-26 14:13:00 +0200

    caching

--- pkgs/by-name/az/azure-cli/extensions-tool.py
+++ pkgs/by-name/az/azure-cli/extensions-tool.py
@@ -2,13 +2,14 @@
 
 import argparse
 import base64
+import datetime
 import json
 import logging
 import os
 import sys
 from dataclasses import dataclass, asdict
 from pathlib import Path
-from typing import Any, Dict, List, Iterable, Optional
+from typing import Any, Dict, Iterable, Optional, Set, Tuple
 from urllib.request import Request, urlopen
 
 from packaging.version import Version, parse
@@ -27,13 +28,67 @@ class Ext:
     hash: str
     description: str
 
-def eprint(*args, **kwargs):
-    logger.error(*args, **kwargs)
+
+class ExtByName(Ext):
+    def __eq__(self, other):
+        return self.pname == other.pname
+
+
+def _get_cached_index(path: Path) -> Tuple[datetime.datetime, Any]:
+    with open(path, "r") as f:
+        data = f.read()
+
+    j = json.loads(data)
+    cache_date_str = j["cache_date"]
+    if cache_date_str:
+        cache_date = datetime.datetime.fromisoformat(cache_date_str)
+    else:
+        cache_date = datetime.datetime.min
+    return cache_date, data
+
+
+def _write_index_to_cache(data: Any, path: Path):
+    j = json.loads(data)
+    j["cache_date"] = datetime.datetime.now().isoformat()
+    with open(path, "w") as f:
+        json.dump(j, f)
+
+
+def _get_remote_index():
+    r = Request(INDEX_URL)
+    with urlopen(r) as resp:
+        return resp.read()
+
+
+def get_extension_index(cache_dir: Path) -> Set[Ext]:
+    index_file = cache_dir / "index.json"
+    os.makedirs(cache_dir, exist_ok=True)
+
+    try:
+        index_cache_date, index_data = _get_cached_index(index_file)
+    except KeyError:
+        logger.info("index has not been cached, downloading from source")
+        logger.info("creating index cache in %s", index_file)
+        _write_index_to_cache(_get_remote_index(), index_file)
+        return get_extension_index(cache_dir)
+
+    if (
+        index_cache_date
+        and datetime.datetime.now() - index_cache_date > datetime.timedelta(days=1)
+    ):
+        logger.info(
+            "cache is outdated (%s), refreshing",
+            datetime.datetime.now() - index_cache_date,
+        )
+        _write_index_to_cache(_get_remote_index(), index_file)
+        return get_extension_index(cache_dir)
+
+    return json.loads(index_data)
 
 
 def get_index(cache_dir: str):
     cache_file = f"{cache_dir}/index.json"
-    eprint(f"Downloading index to {cache_file}")
+    logger.info("Downloading index to %s", cache_file)
     os.makedirs(cache_dir, exist_ok=True)
     with open(cache_file, "wb") as f:
         req = Request(INDEX_URL)
@@ -50,7 +105,8 @@ def _convert_hash_digest_from_hex_to_b64_sri(s: str) -> str:
         logger.error("not a hex value: %s", str(err))
         raise err
 
-    return f"sha256-{base64.b64encode(b).decode("utf-8")}"
+    return f"sha256-{base64.b64encode(b).decode('utf-8')}"
+
 
 def _filter_invalid(o: Dict[str, Any]) -> bool:
     if "metadata" not in o:
@@ -64,7 +120,9 @@ def _filter_invalid(o: Dict[str, Any]) -> bool:
         logger.warning(f"{metadata['name']} without version")
         return False
     if "azext.minCliCoreVersion" not in metadata:
-        logger.warning(f"{metadata['name']} {metadata['version']} does not have azext.minCliCoreVersion")
+        logger.warning(
+            f"{metadata['name']} {metadata['version']} does not have azext.minCliCoreVersion"
+        )
         return False
     if "summary" not in metadata:
         logger.info(f"{metadata['name']} {metadata['version']} without summary")
@@ -84,6 +142,10 @@ def _filter_compatible(o: Dict[str, Any], cli_version: Version) -> bool:
     return cli_version >= minCliVersion
 
 
+def _filter_by_name(e: Ext, ext_name: str) -> bool:
+    return ext_name == e.pname
+
+
 def _transform_dict_to_obj(o: Dict[str, Any]) -> Ext:
     m = o["metadata"]
     return Ext(
@@ -91,23 +153,40 @@ def _transform_dict_to_obj(o: Dict[str, Any]) -> Ext:
         version=parse(m["version"]),
         url=o["downloadUrl"],
         hash=_convert_hash_digest_from_hex_to_b64_sri(o["sha256Digest"]),
-        description=m["summary"]
+        description=m["summary"],
     )
 
+
 def _get_latest_version(versions: Iterable[Ext]) -> Optional[Ext]:
-    logger.debug((versions))
-    v = list(versions).sort(key="version", reverse=True)
-    if v:
-        return v[0]
+    if not versions:
+        return None
 
-    return None
+    return max(versions, key=lambda e: e.version)
 
 
-def processExtension(extVersions: dict, cli_version: Version):
-    extVersions = filter(_filter_invalid, extVersions)
-    extVersions = filter(lambda v: _filter_compatible(v, cli_version), extVersions)
-    extVersionsObj = map(_transform_dict_to_obj, extVersions)
-    return _get_latest_version(extVersionsObj)
+def processExtension(
+    extVersions: dict, cli_version: Version, ext_name: Optional[str] = None
+):
+    versions = filter(_filter_invalid, extVersions)
+    versions = filter(lambda v: _filter_compatible(v, cli_version), versions)
+    versions_obj = map(_transform_dict_to_obj, versions)
+    if ext_name:
+        versions_obj = filter(lambda v: _filter_by_name(v, ext_name), versions_obj)
+
+    return _get_latest_version(versions_obj)
+
+
+def _diff_sets(
+    set_local: Set[Ext], set_remote: Set[Ext]
+) -> Tuple[Set[Ext], Set[Ext], Set[Ext]]:
+    set_local_by_name, set_remote_by_name = set_local, set_remote
+    set_local_by_name.__class__ = ExtByName
+    set_remote_by_name.__class__ = ExtByName
+    return (
+        set_local - set_remote,
+        set_remote - set_local,
+        set.intersection(set_local, set_remote),
+    )
 
 
 def main():
@@ -124,7 +203,7 @@ def main():
         "--cache-dir",
         type=str,
         help="path where to cache the extension index",
-        default=os.getenv("XDG_CACHE_HOME", Path.home() / ".cache")
+        default=Path(os.getenv("XDG_CACHE_HOME", Path.home() / ".cache"))
         / "azure-cli-extensions-tool",
     )
     parser.add_argument(
@@ -135,19 +214,20 @@ def main():
     )
     args = parser.parse_args()
 
-    i = get_index(args.cache_dir)
+    i = get_extension_index(args.cache_dir)
     assert i["formatVersion"] == "1"  # only support formatVersion 1
     extensions = i["extensions"]
 
-    eprint(f"Filtering extensions for azure-cli version {args.cli_version}")
+    logger.info("Filtering extensions for azure-cli version %s", args.cli_version)
     cli_version = parse(args.cli_version)
     for extName, extVersions in extensions.items():
-        extensions[extName] = processExtension(extVersions, cli_version)
+        extension = processExtension(extVersions, cli_version)
+        if extension:
+            extensions[extName] = asdict(extension)
+        else:
+            extensions[extName] = None
 
-    print(json.dumps(
-        [asdict(e) for e in extensions],
-        indent=2
-    ))
+    print(json.dumps(extensions, indent=2, default=str))
 
 
 if __name__ == "__main__":
